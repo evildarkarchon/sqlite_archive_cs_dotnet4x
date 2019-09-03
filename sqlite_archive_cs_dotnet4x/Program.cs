79fe476848efe4ede53bb5fd59d4ba176f0a168d
Andrew Nelson <evildarkarchon@gmail.com>
2019-09-02 21:17:01 -0700

    Create the Visual Studio Project and move files into it.

--- sqlite_archive_cs_dotnet4x/Program.cs
+++ sqlite_archive_cs_dotnet4x/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace sqlite_archive_cs
 {
@@ -6,7 +10,6 @@ namespace sqlite_archive_cs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
